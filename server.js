6c4201853eb7021f29d0bca18c1eaf878f19dd0f
Cayden <114540789+caydenpark@users.noreply.github.com>
2024-02-10 23:10:47 -0700

    SwaggerUI installed

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const routes = require('./routes/index.js')
 
 app.use(express.json());
 app.use('/', routes)
+app.use('/worldCupChampions', routes);
+app.use('/api-docs', swaggerUI.setup(swaggerDocument));
 
 const port = process.env.PORT || 3000;
 
