22dab8079217968729d32eb8f58f104f1004d73a
Cayden <114540789+caydenpark@users.noreply.github.com>
2024-03-02 22:59:14 -0700

    delete route for Hello World message

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 router.use('/worldCupChampions', require('./worldCupChampions'));
 
 module.exports = router;
