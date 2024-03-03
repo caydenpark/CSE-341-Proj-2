5c1d1e15c83c8cafd8bda92273ac309ee5ae55ac
Cayden <114540789+caydenpark@users.noreply.github.com>
2024-03-02 23:17:13 -0700

    Add authentication middleware and update routes

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
+require('dotenv').config();
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const express = require('express')
 const app = express();
+const { auth, requiresAuth } = require('express-openid-connect');
 
 const routes = require('./routes/index.js')
-const oauthRoute = require('./routes/oauth.js');
+
+const port = process.env.PORT || 3000;
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 app.use(express.json());
 app.use('/', routes)
 app.use('/worldCupChampions', routes);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
-app.use(express.static('static'));
-app.use('/oauth', oauthRoute);
-
-const port = process.env.PORT || 3000;
 
 app.listen(process.env.PORT || port);
 console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
